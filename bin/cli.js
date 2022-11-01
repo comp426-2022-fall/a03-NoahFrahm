0d0f8ae82b29af2a389b8c6993776da49230850f
NoahFrahm <nofrahm@live.unc.edu>
2022-11-01 14:11:30 -0400

    took in user args
    setup minimist to accept user args

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import roll from "../lib/roll.js"
+import minimist from "minimist";
 
-let results = roll(6, 2, 10);
-console.log(results);
-console.log("ran");
+
+// parse command line args
+const args = minimist(process.argv.slice(2))
+let sides = args.sides ? args.sides: 6;
+let dice = args.dice ? args.dice: 2;
+let rolls = args.rolls ? args.rolls: 6;
+
+
+let results = roll(sides, dice, rolls);
+console.log(JSON.stringify(results));
