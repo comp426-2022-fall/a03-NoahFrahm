7c187681187e51dded01658e58c9647df3c0aa0d
NoahFrahm <nofrahm@live.unc.edu>
2022-11-01 14:48:35 -0400

    negative input
    added edge case for negative inputs

--- bin/cli.js
+++ bin/cli.js
@@ -4,10 +4,11 @@ import minimist from "minimist";
 
 
 // parse command line args
+// add checks for negative inputs
 const args = minimist(process.argv.slice(2))
-let sides = args.sides ? args.sides: 6;
-let dice = args.dice ? args.dice: 2;
-let rolls = args.rolls ? args.rolls: 1;
+let sides = args.sides && args.sides > 0 ? args.sides: 6;
+let dice = args.dice && args.sides ? args.dice: 2;
+let rolls = args.rolls && args.sides ? args.rolls: 1;
 
 
 let results = roll(sides, dice, rolls);
